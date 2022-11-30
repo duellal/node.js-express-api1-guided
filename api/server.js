a3985526ad46b80c9821893d8b613653ce946531
duellal <alexandriaduell@gmail.com>
2022-11-30 12:31:39 -0500

    first commit - need to install httpie first before continuing

--- api/server.js
+++ api/server.js
@@ -1,16 +1,35 @@
 // IMPORTS AT THE TOP
+const express = require('express')
 
 // INSTANCE OF EXPRESS APP
+const server = express()
 
 // GLOBAL MIDDLEWARE
+server.use(express.json())
 
 // ENDPOINTS
+server.get('/hello-world', (req, res) => {
+   res.status(200).json({ message: 'Hello world!' })
+})
 
 // [GET]    /             (Hello World endpoint)
+
+
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+
+
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
+
+
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
+
+
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
+
+
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
+
+
 // EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // import the server and start it!
+const server = require('./api/server')
+
+//launch the server
+server.listen(9000, () => {
+   console.log(`Server started on http://localhost:9000`)
+})
+
+// cannot run http :9000/hello-world b/c I do not have httpie installed - need brew or macWorld first to install httpie for some reason
